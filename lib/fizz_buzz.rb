84d039b0121d601584bb8c8adf5b24160ce3253a
FleurTamboezer <ftamboezer@gmail.com>
2018-01-09 14:22:16 +0100

    fizz_buzz works including sad path

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,5 +1,19 @@
 # lib/fizz_buzz.rb
 
 def fizz_buzz(number)
+
+  if number == String
+    'You should enter a number'
+
+  elsif number < 0
+  'Wrong input'
+  elsif number % 15 == 0
+   'fizz_buzz'
+  elsif number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
   number
+  end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,13 +2,30 @@
 
 require './lib/fizz_buzz'
 
-
-
 describe 'fizz_buzz' do
 
-it 'returns 1 if number is 1' do
-expect(fizz_buzz(1)).to eq 1
+  it 'returns 1 if number is 1' do
+  expect(fizz_buzz(1)).to eq 1
+    end
 
+  it "returns 'fizz' if number is divisible by 3" do
+    expect(fizz_buzz(3)).to eq 'fizz'
+    end
 
-end
+  it "returns 'buzz' if number is divisible by 5" do
+    expect(fizz_buzz(5)).to eq 'buzz'
+    end
+
+  it "returns 'fizz_buzz' if number is divisible by 3 and 5" do
+    expect(fizz_buzz(15)).to eq 'fizz_buzz'
+  end
+
+  it "returns 'Wrong input' if number is not a positive number" do
+    expect(fizz_buzz(-1)).to eq 'Wrong input'
+  end
+
+  it "returns 'You should enter a number' if number is a string" do
+    expect(fizz_buzz(String)).to eq 'You should enter a number'
+  end
+
 end
